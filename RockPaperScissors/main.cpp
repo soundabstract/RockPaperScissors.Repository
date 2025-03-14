19106a4eea260437b9095c41defdccb6035612ba
soundabstract <93556701+soundabstract@users.noreply.github.com>
2025-03-14 00:56:17 -0500

    Update main.cpp
    Removed unnecessary comments

--- RockPaperScissors/main.cpp
+++ RockPaperScissors/main.cpp
@@ -1,12 +1,9 @@
-// RockPaperScissors.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "aiComputer.h"
 #include "rpsShoot.h"
 
-//best 2 out of 3
-
+//Plays a rock paper scissors game between the user and computer
+//Best 2 out of 3
 int main()
 {
 
@@ -69,19 +66,5 @@ int main()
 
 	}
 
-
 	return 0;
 }
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
